6ce7d8774711bcb54434ab54b427f0ecda0cc309
Daniil <daniilknopik05@gmail.com>
2025-11-25 16:46:42 +0200

    Realization Test 1

--- DeliverySystem_/Class1.cs
+++ DeliverySystem_/Class1.cs
@@ -2,6 +2,23 @@
 {
     public class Class1
     {
+        public enum OrderStatus
+        {
+            Pending,
+            Shipped,
+            Delivered
+        }
 
+        public class Order
+        {
+            public decimal Price { get; private set; }
+            public OrderStatus Status { get; private set; }
+
+            public Order(decimal price)
+            {
+                Price = price;
+                Status = OrderStatus.Pending;
+            }
+        }
     }
 }
