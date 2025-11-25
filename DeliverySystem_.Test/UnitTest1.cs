3eeee74a1fefce5e329fcb54721d9981eea3203c
Daniil <daniilknopik05@gmail.com>
2025-11-25 16:50:15 +0200

    Created Test 2

--- DeliverySystem_.Test/UnitTest1.cs
+++ DeliverySystem_.Test/UnitTest1.cs
@@ -9,5 +9,15 @@ namespace DeliverySystem_.Test
 
             Assert.Equal(OrderStatus.Pending, order.Status);
         }
+
+        [Fact]
+        public void ChangeStatus_ValidStatus_UpdatesStatus()
+        {
+            var order = new Order(100);
+
+            order.ChangeStatus(OrderStatus.Shipped);
+
+            Assert.Equal(OrderStatus.Shipped, order.Status);
+        }
     }
 }
